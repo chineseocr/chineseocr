7cfa280c7e081952b9475183da81dfcdcb0bac6a
wenlihaoyu <wenyinlong52@163.com>
2019-08-17 12:17:12 +0800

    解决多请求报错、内存耗尽文件

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 import time
 import web
 import numpy as np
+import uuid
 from PIL import Image
 web.config.debug  = True
 
@@ -117,7 +118,10 @@ class OCR:
         return render.ocr(post)
 
     def POST(self):
+        t = time.time()
         data = web.data()
+        uidJob = uuid.uuid1().__str__()
+        filelock='file.lock'
         data = json.loads(data)
         billModel = data.get('billModel','')
         textAngle = data.get('textAngle',False)##文字检测
@@ -129,58 +133,68 @@ class OCR:
             img = np.array(img)
             
         H,W = img.shape[:2]
-        timeTake = time.time()
-        if textLine:
-            ##单行识别
-            partImg = Image.fromarray(img)
-            text    = ocr.predict(partImg.convert('L'))
-            res =[ {'text':text,'name':'0','box':[0,0,W,0,W,H,0,H]} ]
-        else:
-            detectAngle = textAngle
-            result,angle= model.model(img,
-                                        scale=scale,
-                                        maxScale=maxScale,
-                                        detectAngle=detectAngle,##是否进行文字方向检测，通过web传参控制
-                                        MAX_HORIZONTAL_GAP=100,##字符之间的最大间隔，用于文本行的合并
-                                        MIN_V_OVERLAPS=0.6,
-                                        MIN_SIZE_SIM=0.6,
-                                        TEXT_PROPOSALS_MIN_SCORE=0.1,
-                                        TEXT_PROPOSALS_NMS_THRESH=0.3,
-                                        TEXT_LINE_NMS_THRESH = 0.99,##文本行之间测iou值
-                                        LINE_MIN_SCORE=0.1,
-                                        leftAdjustAlph=0.01,##对检测的文本行进行向左延伸
-                                        rightAdjustAlph=0.01,##对检测的文本行进行向右延伸
-                                       )
 
-
-
-            if billModel=='' or billModel=='通用OCR' :
-                result = union_rbox(result,0.2)
-                res = [{'text':x['text'],
-                        'name':str(i),
-                        'box':{'cx':x['cx'],
-                               'cy':x['cy'],
-                               'w':x['w'],
-                               'h':x['h'],
-                               'angle':x['degree']
-
-                              }
-                       } for i,x in enumerate(result)]
-                res = adjust_box_to_origin(img,angle, res)##修正box
-
-            elif billModel=='火车票':
-                res = trainTicket.trainTicket(result)
-                res = res.res
-                res =[ {'text':res[key],'name':key,'box':{}} for key in res]
-
-            elif billModel=='身份证':
-
-                res = idcard.idcard(result)
-                res = res.res
-                res =[ {'text':res[key],'name':key,'box':{}} for key in res]
+        while time.time()-t<=TIMEOUT:
+            if os.path.exists(filelock):
+                continue
+            else:
+                with open(filelock,'w') as f:
+                    f.write(uidJob)
+                                                
+                if textLine:
+                    ##单行识别
+                    partImg = Image.fromarray(img)
+                    text    = ocr.predict(partImg.convert('L'))
+                    res =[ {'text':text,'name':'0','box':[0,0,W,0,W,H,0,H]} ]
+                else:
+                    detectAngle = textAngle
+                    result,angle= model.model(img,
+                                                scale=scale,
+                                                maxScale=maxScale,
+                                                detectAngle=detectAngle,##是否进行文字方向检测，通过web传参控制
+                                                MAX_HORIZONTAL_GAP=100,##字符之间的最大间隔，用于文本行的合并
+                                                MIN_V_OVERLAPS=0.6,
+                                                MIN_SIZE_SIM=0.6,
+                                                TEXT_PROPOSALS_MIN_SCORE=0.1,
+                                                TEXT_PROPOSALS_NMS_THRESH=0.3,
+                                                TEXT_LINE_NMS_THRESH = 0.99,##文本行之间测iou值
+                                                LINE_MIN_SCORE=0.1,
+                                                leftAdjustAlph=0.01,##对检测的文本行进行向左延伸
+                                                rightAdjustAlph=0.01,##对检测的文本行进行向右延伸
+                                               )
+        
+        
+        
+                    if billModel=='' or billModel=='通用OCR' :
+                        result = union_rbox(result,0.2)
+                        res = [{'text':x['text'],
+                                'name':str(i),
+                                'box':{'cx':x['cx'],
+                                       'cy':x['cy'],
+                                       'w':x['w'],
+                                       'h':x['h'],
+                                       'angle':x['degree']
+        
+                                      }
+                               } for i,x in enumerate(result)]
+                        res = adjust_box_to_origin(img,angle, res)##修正box
+        
+                    elif billModel=='火车票':
+                        res = trainTicket.trainTicket(result)
+                        res = res.res
+                        res =[ {'text':res[key],'name':key,'box':{}} for key in res]
+        
+                    elif billModel=='身份证':
+        
+                        res = idcard.idcard(result)
+                        res = res.res
+                        res =[ {'text':res[key],'name':key,'box':{}} for key in res]
+                        
+                    os.remove(filelock)
+                    break
             
         
-        timeTake = time.time()-timeTake
+        timeTake = time.time()-t
          
         return json.dumps({'res':res,'timeTake':round(timeTake,4)},ensure_ascii=False)
         
